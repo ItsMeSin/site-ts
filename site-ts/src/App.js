815171d9a6a651bbaef2cd4b935c8473162ea328
kylian masquelez <kmqlz72@gmail.com>
2025-10-15 16:49:31 +0200

    Fin de projet

--- site-ts/src/App.js
+++ site-ts/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import ArtisansCouvreurSite from "./ArtisansCouvreurSite";
 import AdminDashboard from "./AdminDashboard";
 import LoginAdmin from "./LoginAdmin";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/admin");
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ArtisansCouvreurSite />} />
-        <Route path="/admin" element={<LoginAdmin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<ArtisansCouvreurSite />} />
+      <Route path="/admin" element={<LoginAdmin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard onLogout={handleLogout} />} />
+    </Routes>
   );
 }
 
